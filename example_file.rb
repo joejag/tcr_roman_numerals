6d9636a9c8be7184683e3e35df565844feca35b5
Joe Wright <joe@joejag.com>
2019-03-14 23:46:22 +0000

    working

--- example_file.rb
+++ example_file.rb
@@ -18,10 +18,9 @@ end
 
 class NumbersTest < Minitest::Test
 
-  def test_roman
+  def test_roman_low
     examples = { 'I' => 1, 'II' => 2, 'III' => 3, 'IV' => 4, 'V' => 5, 
     'VI' => 6, 'VII' => 7, 'VIII' => 8, 'IX' => 9, 'X' => 10 }
-    
     examples.each {|k,v| assert_equal k, Roman.new.convert(v) }
   end
   

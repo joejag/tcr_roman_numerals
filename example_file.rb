d83d2d5891302ee223d83e7f8e53638697b2d23c
Joe Wright <joe@joejag.com>
2019-03-14 23:42:51 +0000

    working

--- example_file.rb
+++ example_file.rb
@@ -11,7 +11,7 @@ end
 class Roman
   def convert(number)
     base = {1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV', 5 => 'V',
-            6 => 'VI', 7 => 'VII'}
+            6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX', 10 => 'X'}
     base[number]
   end
 end
@@ -26,9 +26,9 @@ class NumbersTest < Minitest::Test
     assert_equal 'V', Roman.new.convert(5)
     assert_equal 'VI', Roman.new.convert(6)
     assert_equal 'VII', Roman.new.convert(7)
-    # assert_equal 'VIII', Roman.new.convert(8)
-    # assert_equal 'IV', Roman.new.convert(9)
-    # assert_equal 'X', Roman.new.convert(10)
+    assert_equal 'VIII', Roman.new.convert(8)
+    assert_equal 'IX', Roman.new.convert(9)
+    assert_equal 'X', Roman.new.convert(10)
   end
   
   def test_urnfield

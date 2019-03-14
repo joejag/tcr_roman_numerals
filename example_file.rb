f092687b1ff253e6a26964963744d006a81eaf59
Joe Wright <joe@joejag.com>
2019-03-14 23:48:21 +0000

    working

--- example_file.rb
+++ example_file.rb
@@ -14,7 +14,7 @@ class Roman
     base = {1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV', 5 => 'V',
             6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX', 10 => 'X'}
     return base[number] if base.has_key? number
-    ''
+    'NaN'
   end
 end
 

399ea7816ecc8ae2b7e512590fe8eb12cb1f921c
Joe Wright <joe@joejag.com>
2019-03-14 23:14:41 +0000

    working

--- example_file.rb
+++ example_file.rb
@@ -10,8 +10,8 @@ class NumbersTest < Minitest::Test
 
   def test_urnfield
     assert '/' == Urnfield.new.convert(1)
-    assert "//" == Urnfield.new.convert(2)
-    assert "///" == Urnfield.new.convert(3)
-    assert "////" == Urnfield.new.convert(4)
+    assert '//' == Urnfield.new.convert(2)
+    assert '///' == Urnfield.new.convert(3)
+    assert '////' == Urnfield.new.convert(4)
   end
 end

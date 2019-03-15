569c75bcd3c1b09fd4e5351991335e5dcd86fec5
Joe Wright <joe@joejag.com>
2019-03-15 00:10:26 +0000

    working

--- example_file.rb
+++ example_file.rb
@@ -13,7 +13,7 @@ class Roman
     base = {1 => 'I', 2 => 'II', 3 => 'III', 
             4 => 'IV', 5 => 'V', 6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX', 10 => 'X', 
             40 => 'XL', 50 => 'L', 90 => 'XC', 100 => 'C', 
-            400 => 'CD', 500 => 'D'}
+            400 => 'CD', 500 => 'D', 900 => 'CM', 1000 => 'M'}
     return base[number] if base.has_key? number
 
     answer = ''

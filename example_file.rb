b0c8add30fb2872d048b754b94534451e7099839
Joe Wright <joe@joejag.com>
2019-03-15 00:09:56 +0000

    working

--- example_file.rb
+++ example_file.rb
@@ -10,9 +10,10 @@ end
 
 class Roman
   def convert(number)
-    base = {1 => 'I', 2 => 'II', 3 => 'III', 4 => 'IV', 5 => 'V',
-            6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX', 10 => 'X', 
-            40 => 'XL', 50 => 'L', 90 => 'XC', 100 => 'C'}
+    base = {1 => 'I', 2 => 'II', 3 => 'III', 
+            4 => 'IV', 5 => 'V', 6 => 'VI', 7 => 'VII', 8 => 'VIII', 9 => 'IX', 10 => 'X', 
+            40 => 'XL', 50 => 'L', 90 => 'XC', 100 => 'C', 
+            400 => 'CD', 500 => 'D'}
     return base[number] if base.has_key? number
 
     answer = ''
